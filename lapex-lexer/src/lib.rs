3ade77b48765041fabc9d7e70b53df9600cdfdfc
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2022-09-17 12:11:40 +0200

    implemented powerset construction

--- lapex-lexer/src/lib.rs
+++ lapex-lexer/src/lib.rs
@@ -1,11 +1,19 @@
 use std::{
-    collections::{BTreeSet, HashSet},
+    collections::{BTreeSet, HashMap, HashSet},
     fmt::Debug,
+    hash::Hash,
     ops::RangeInclusive,
 };
 
 use lapex_input::{Characters, Pattern, TokenRule};
-use petgraph::{graph::EdgeIndex, graph::NodeIndex, prelude::DiGraph, Graph};
+use petgraph::{
+    data::Build,
+    graph::EdgeIndex,
+    graph::NodeIndex,
+    prelude::DiGraph,
+    visit::{EdgeRef, IntoNodeReferences},
+    Graph,
+};
 
 enum Connection {
     Epsilon,
@@ -61,7 +69,7 @@ impl Nfa {
     }
 }
 
-fn build_pattern(
+fn build_nfa_from_pattern(
     start: NodeIndex,
     end: NodeIndex,
     alphabet: &Alphabet,
@@ -74,17 +82,17 @@ fn build_pattern(
                 let mut start = start;
                 for pat in &elements[..elements.len() - 1] {
                     let end = nfa.add_tmp();
-                    build_pattern(start, end, alphabet, nfa, pat);
+                    build_nfa_from_pattern(start, end, alphabet, nfa, pat);
                     start = end;
                 }
-                build_pattern(start, end, alphabet, nfa, elements.last().unwrap());
+                build_nfa_from_pattern(start, end, alphabet, nfa, elements.last().unwrap());
             }
         }
         Pattern::Alternative { elements } => {
             for elem in elements {
                 let inner_start = nfa.add_tmp();
                 let inner_end = nfa.add_tmp();
-                build_pattern(inner_start, inner_end, alphabet, nfa, elem);
+                build_nfa_from_pattern(inner_start, inner_end, alphabet, nfa, elem);
                 nfa.add_edge_epsilon(start, inner_start);
                 nfa.add_edge_epsilon(inner_end, end);
             }
@@ -92,7 +100,7 @@ fn build_pattern(
         Pattern::Optional { inner } => {
             let inner_start = nfa.add_tmp();
             let inner_end = nfa.add_tmp();
-            build_pattern(inner_start, inner_end, alphabet, nfa, inner);
+            build_nfa_from_pattern(inner_start, inner_end, alphabet, nfa, inner);
             nfa.add_edge_epsilon(start, end);
             nfa.add_edge_epsilon(start, inner_start);
             nfa.add_edge_epsilon(inner_end, end);
@@ -100,7 +108,7 @@ fn build_pattern(
         Pattern::OneOrMany { inner } => {
             let inner_start = nfa.add_tmp();
             let inner_end = nfa.add_tmp();
-            build_pattern(inner_start, inner_end, alphabet, nfa, inner);
+            build_nfa_from_pattern(inner_start, inner_end, alphabet, nfa, inner);
             nfa.add_edge_epsilon(start, inner_start);
             nfa.add_edge_epsilon(inner_end, end);
             nfa.add_edge_epsilon(inner_end, inner_start);
@@ -108,7 +116,7 @@ fn build_pattern(
         Pattern::ZeroOrMany { inner } => {
             let inner_start = nfa.add_tmp();
             let inner_end = nfa.add_tmp();
-            build_pattern(inner_start, inner_end, alphabet, nfa, inner);
+            build_nfa_from_pattern(inner_start, inner_end, alphabet, nfa, inner);
             nfa.add_edge_epsilon(start, end);
             nfa.add_edge_epsilon(start, inner_start);
             nfa.add_edge_epsilon(inner_end, end);
@@ -251,6 +259,142 @@ fn generate_alphabet(rules: &Vec<TokenRule>) -> Alphabet {
     Alphabet { ranges }
 }
 
+fn epsilon_closure(
+    graph: &Graph<State, Connection>,
+    start_nodes: Vec<NodeIndex>,
+    nodes: &mut HashSet<NodeIndex>,
+) {
+    for start_node in start_nodes {
+        nodes.insert(start_node);
+        let edges = graph.edges_directed(start_node, petgraph::Direction::Outgoing);
+        for edge in edges {
+            if let Connection::Epsilon = edge.weight() {
+                let target = edge.target();
+                if !nodes.contains(&target) {
+                    epsilon_closure(graph, vec![target], nodes);
+                }
+            }
+        }
+    }
+}
+
+fn add_powerset_to_dfa(
+    nfa: &Graph<State, Connection>,
+    dfa: &mut Graph<HashSet<NodeIndex>, usize>,
+    nodes: Vec<NodeIndex>,
+) -> NodeIndex {
+    let mut closure = HashSet::new(); // TODO: test perf of different data structures
+    epsilon_closure(&nfa, nodes, &mut closure);
+
+    // find an existing node with the same powerset
+    let node_dfa_opt: Option<NodeIndex> = dfa
+        .node_references()
+        .find(|(_, w)| w == &&closure)
+        .map(|(i, _)| i);
+    if let Some(node_dfa) = node_dfa_opt {
+        // if the powerset exists, no need to recompute
+        node_dfa
+    } else {
+        // if the powerset is new, add it to the graph and recurse
+        let node_dfa = dfa.add_node(closure.clone());
+
+        let mut target_multi_map: HashMap<usize, Vec<NodeIndex>> = HashMap::new();
+        for node in closure {
+            let edges = nfa.edges_directed(node, petgraph::Direction::Outgoing);
+            for edge in edges {
+                if let Connection::Alphabet(c) = edge.weight() {
+                    let target = edge.target();
+                    target_multi_map
+                        .entry(*c)
+                        .or_insert(Vec::new())
+                        .push(target);
+                }
+            }
+        }
+        for (c, targets) in target_multi_map {
+            let target_dfa = add_powerset_to_dfa(nfa, dfa, targets);
+            dfa.add_edge(node_dfa, target_dfa, c);
+        }
+        node_dfa
+    }
+}
+
+enum DfaState {
+    Temporary { id: usize },
+    Accepting { accepts: Vec<String> },
+}
+
+impl Debug for DfaState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Temporary { id } => write!(f, "S{}", id),
+            Self::Accepting { accepts } => write!(f, "{:?}", accepts),
+        }
+    }
+}
+
+fn convert_powerset_to_dfa(
+    powerset_dfa: &Graph<HashSet<NodeIndex>, usize>,
+    nfa: &Graph<State, Connection>,
+    tmp_id: &mut usize,
+    dfa: &mut Graph<DfaState, usize>,
+    visited: &mut HashMap<NodeIndex, NodeIndex>,
+    node: NodeIndex,
+) -> NodeIndex {
+    let mut accepts = Vec::new();
+    let powerset = powerset_dfa.node_weight(node).unwrap();
+    for nfa_index in powerset {
+        let state = nfa.node_weight(*nfa_index);
+        match state {
+            Some(State::Final { token }) => {
+                accepts.push(token.clone());
+            }
+            _ => (),
+        }
+    }
+    let start = if !accepts.is_empty() {
+        dfa.add_node(DfaState::Accepting { accepts })
+    } else {
+        *tmp_id += 1;
+        dfa.add_node(DfaState::Temporary { id: *tmp_id })
+    };
+    visited.insert(node, start);
+
+    for edge in powerset_dfa.edges_directed(node, petgraph::Direction::Outgoing) {
+        let end = if let Some(end) = visited.get(&edge.target()) {
+            *end
+        } else {
+            convert_powerset_to_dfa(powerset_dfa, nfa, tmp_id, dfa, visited, edge.target())
+        };
+        dfa.add_edge(start, end, *edge.weight());
+    }
+    start
+}
+
+fn powerset_construction(
+    nfa: &Graph<State, Connection>,
+    start: NodeIndex,
+) -> Graph<DfaState, usize> {
+    let mut powerset_dfa: Graph<HashSet<NodeIndex>, usize> = DiGraph::new();
+
+    let start_dfa = add_powerset_to_dfa(nfa, &mut powerset_dfa, vec![start]);
+
+    let mut tmp_id = 0;
+    let mut dfa: Graph<DfaState, usize> = DiGraph::new();
+
+    let mut visited = HashMap::new();
+    convert_powerset_to_dfa(
+        &powerset_dfa,
+        nfa,
+        &mut tmp_id,
+        &mut dfa,
+        &mut visited,
+        start_dfa,
+    );
+
+    dfa
+}
+
 pub fn build_dfa(rules: Vec<TokenRule>) {
     let mut nfa = Nfa {
         graph: DiGraph::new(),
@@ -265,9 +409,13 @@ pub fn build_dfa(rules: Vec<TokenRule>) {
 
     let start = nfa.add_tmp();
     for rule in rules {
-        let end = nfa.add_final(rule.token());
-        build_pattern(start, end, &alphabet, &mut nfa, rule.pattern());
+        let rule_start = nfa.add_tmp();
+        let rule_end = nfa.add_final(rule.token());
+        nfa.add_edge_epsilon(start, rule_start);
+        build_nfa_from_pattern(rule_start, rule_end, &alphabet, &mut nfa, rule.pattern());
     }
 
-    println!("{:?}", petgraph::dot::Dot::with_config(&nfa.graph, &[]));
+    let dfa = powerset_construction(&nfa.graph, start);
+    // println!("{:?}", petgraph::dot::Dot::with_config(&nfa.graph, &[]));
+    println!("{:?}", petgraph::dot::Dot::with_config(&dfa, &[]));
 }
